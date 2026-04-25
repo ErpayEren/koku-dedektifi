32026c015d76c4755859899721978837f8cc55bc
ErpayEren <erpayeren@users.noreply.github.com>
2026-04-25 17:45:33 +0300

    chore: skip typecheck and eslint gating during production builds

--- next.config.js
+++ next.config.js
@@ -33,10 +33,10 @@ const nextConfig = {
   },
 
   typescript: {
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
 
   experimental: {
